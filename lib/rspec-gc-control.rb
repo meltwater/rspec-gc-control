e4081dbb943ef8d084777c08f43640a9296589a7
the-architect <marcel.scherf@epicteams.com>
2016-10-12 16:51:28 +0200

    remove custom formatter

--- lib/rspec-gc-control.rb
+++ lib/rspec-gc-control.rb
@@ -1,6 +1,4 @@
-$:.unshift(File.expand_path("..", __FILE__))
+$:.unshift(File.expand_path('..', __FILE__))
 
 require 'rspec-gc-control/configuration'
 require 'rspec-gc-control/example'
-require 'rspec-gc-control/base_text_formatter'
-
