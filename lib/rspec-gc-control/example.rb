cd6a357df2ffeeedd42d41fa6b2d3a5e42f690a3
the-architect <marcel.scherf@epicteams.com>
2016-10-12 16:52:24 +0200

    we only support rspec3 now

--- lib/rspec-gc-control/example.rb
+++ lib/rspec-gc-control/example.rb
@@ -5,32 +5,15 @@ module RSpec
     # Extensions to RSpec::Core::Example to perform explicit GC and capture
     # GC execution time.
     class Example
-    private
+      private
 
-      if defined?(::RSpec::Core) && ::RSpec::Core::Version::STRING >= '3.0.0'
-        alias_method :run_after_example_without_gc, :run_after_example
+      alias_method :run_after_example_without_gc, :run_after_example
 
-        def run_after_example
-          run_after_example_without_gc
-        ensure
-          RSpec.configuration.gc_if_needed
-        end
-      else
-        undef :record_finished
-        def record_finished(status, results={})
-          finished_at = Time.now
-          record results.merge(:status => status, :finished_at => finished_at, :run_time => (finished_at - (execution_result[:started_at] + (execution_result[:gc_time] || 0))))
-        end
-
-        alias_method :run_after_each_without_gc, :run_after_each
-
-        def run_after_each
-          run_after_each_without_gc
-        ensure
-          execution_result[:gc_time] = RSpec.configuration.gc_if_needed
-        end
+      def run_after_example
+        run_after_example_without_gc
+      ensure
+        RSpec.configuration.gc_if_needed
       end
-
     end
   end
 end
